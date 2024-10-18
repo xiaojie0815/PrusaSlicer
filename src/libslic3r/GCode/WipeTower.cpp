b4ae892bae5e3e163f45ce27a3cfd1866f941f68
Lukas Matena <lukasmatena@seznam.cz>
2024-10-18 16:12:22 +0200

    Follow-up to previous commit

--- src/libslic3r/GCode/WipeTower.cpp
+++ src/libslic3r/GCode/WipeTower.cpp
@@ -545,7 +545,6 @@ WipeTower::WipeTower(const Vec2f& pos, double rotation_deg, const PrintConfig& c
     m_semm(config.single_extruder_multi_material.value),
     m_wipe_tower_pos(pos),
     m_wipe_tower_width(float(config.wipe_tower_width)),
-    m_wipe_tower_rotation_angle(rotation_deg),
     m_wipe_tower_brim_width(float(config.wipe_tower_brim_width)),
     m_wipe_tower_cone_angle(float(config.wipe_tower_cone_angle)),
     m_extra_flow(float(config.wipe_tower_extra_flow/100.)),
